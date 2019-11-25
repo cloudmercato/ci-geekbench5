9fe79bcdf46b2bcacf4ad9d2c8f54db57d8996ef
Anthony Monthe <anthony.monthe@gmail.com>
2019-11-25 16:01:07 +0000

    Added Circle

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-ci-base/install_python.sh
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
-    wget -q http://cdn.geekbench.com/Geekbench-5.0.0-Linux.tar.gz
-    tar xf Geekbench-5.0.0-Linux.tar.gz
-    ln -s $(pwd)/Geekbench-5.0.0-Linux/geekbench5 $(pwd)/geekbench5
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]] ; then
-    wget -q http://cdn.geekbench.com/Geekbench-5.0.4-Mac.zip
-    unzip -q Geekbench-5.0.4-Mac.zip
-    ln -s $(pwd)/Geekbench\ 5.app/Contents/Resources/geekbench5 $(pwd)/geekbench5
-fi
-$(pwd)/geekbench5 --unlock $geekbench_email $geekbench_key
+./${1}_install.sh
+~/geekbench5 --unlock $geekbench_email $geekbench_key
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 iterations=${iterations:-3}
 for i in $(seq 1 $iterations) ; do
-    ./geekbench5 --no-upload --export-json output-$i.json
+    ~/geekbench5 --no-upload --export-json output-$i.json
     cb-client geekbench5 < output-$i.json
 done
