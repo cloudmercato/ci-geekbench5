08d85fdd00e491b1420163528fe090446ae179f9
Anthony Monthe <anthony.monthe@gmail.com>
2020-01-19 23:40:07 +0000

    Added gitlab

---
+++ gitlabci_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -q http://cdn.geekbench.com/Geekbench-5.1.0-Linux.tar.gz
+tar xf Geekbench-5.1.0-Linux.tar.gz
+ln -s $(pwd)/Geekbench-5.1.0-Linux/geekbench5 ~/geekbench5
--- semaphore_install.sh
+++ semaphore_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$SEMAPHORE_AGENT_MACHINE_OS_IMAGE" == "ubuntu"* ]] ; then
-    wget -q http://cdn.geekbench.com/Geekbench-5.0.0-Linux.tar.gz
-    tar xf Geekbench-5.0.0-Linux.tar.gz
-    ln -s $(pwd)/Geekbench-5.0.0-Linux/geekbench5 ~/geekbench5
+    wget -q http://cdn.geekbench.com/Geekbench-5.1.0-Linux.tar.gz
+    tar xf Geekbench-5.1.0-Linux.tar.gz
+    ln -s $(pwd)/Geekbench-5.1.0-Linux/geekbench5 ~/geekbench5
 elif [[ "$SEMAPHORE_AGENT_MACHINE_OS_IMAGE" == "macos"* ]] ; then
     wget -q http://cdn.geekbench.com/Geekbench-5.0.4-Mac.zip
     unzip -o -q Geekbench-5.0.4-Mac.zip
